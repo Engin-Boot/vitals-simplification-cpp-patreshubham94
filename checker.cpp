9ce083ba164cefecf6a362ea0749d106c9b5bafe
patreshubham94 <68467015+patreshubham94@users.noreply.github.com>
2020-08-27 18:55:57 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -7,13 +7,17 @@ const int spo2Llimit = 90;
 const int respRateUlimit = 95;
 const int respRateLlimit = 30;
 
-bool vitalsOk(float value, float upper_limit, float lower_limit) {
+bool vitalisOk(float value, float upper_limit, float lower_limit) {
 	return (value >= lower_limit && value <= upper_limit);
 }
+bool vitalsAreOk(float bpm, float spo2, float respRate) {
+	return (vitalisOk(bpm,bpmUlimit,bpmLlimit) && vitalisOk(spo2, spo2Ulimit,spo2Llimit) && vitalisOk(respRate,respRateUlimit, respRateLlimit ) );
+}
+
 
 int main() {
-	assert(vitalsOk(80, respRateUlimit, respRateLlimit) == true);
-	assert(vitalsOk(60, spo2Ulimit, spo2Llimit) == false);
-	assert(vitalsOk(120, bpmUlimit, bpmLlimit) == true);
-	assert(vitalsOk(95, spo2Ulimit, spo2Llimit) == true);
+	assert(vitalsAreOk(80, 95, 40) == true);
+	assert(vitalsAreOk(60, 60, 65) == false);
+	assert(vitalsAreOk(120, 92, 70) == true);
+	assert(vitalsAreOk(95, 98, 85) == true);
 }
